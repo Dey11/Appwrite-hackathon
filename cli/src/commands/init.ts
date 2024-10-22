8ec42a2f4a8c004507611d1743479a7810128436
Dey11 <shreyandey11@gmail.com>
2024-10-22 16:23:08 +0530

    add more config options, implement dependency installation, better error handling

--- cli/src/commands/init.ts
+++ cli/src/commands/init.ts
@@ -1,15 +1,15 @@
 import chalk from "chalk"
-import { Command, program } from "commander"
-import { mkdirSync, writeFileSync, existsSync } from "fs"
+import { Command } from "commander"
+import { existsSync } from "fs"
 import { resolve } from "path"
-import inquirer from "inquirer"
+import createConfigFile from "../utils/create-config"
 
 export const init = new Command("init")
   .description("creates the config file")
   .option("-f, --force", "overwrite existing config file")
   .action(async (options) => {
     if (options.force) {
-      console.log(chalk.yellow(`Overwriting existing config file`))
+      console.log(chalk.yellow(`Overwriting the existing config file, if any.`))
     } else {
       const baseDir = existsSync(resolve(process.cwd(), "form.config.json"))
         ? true
@@ -17,31 +17,11 @@ export const init = new Command("init")
 
       if (baseDir) {
         console.log(
-          chalk.red(`Config file already exists. Use -f to overwrite`),
+          chalk.red(`Config file already exists. Use -f to overwrite.`),
         )
         return
       }
     }
 
-    console.log(chalk.yellow(`Please choose your preferences`))
-
-    const { fileType } = await inquirer.prompt([
-      {
-        type: "list",
-        name: "fileType",
-        message: "What file extension should the component have?",
-        choices: ["js", "ts"],
-      },
-    ])
-
-    const configPref = {
-      fileType: fileType,
-    }
-
-    const dirPath = resolve(process.cwd(), "")
-
-    mkdirSync(dirPath, { recursive: true })
-    const filePath = resolve(dirPath, `form.config.json`)
-    writeFileSync(filePath, JSON.stringify(configPref))
-    console.log(chalk.green(`form.config.json created in the root directory`))
+    createConfigFile()
   })
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -3,9 +3,11 @@
 import { Command } from "commander"
 import { add } from "./commands/add"
 import { init } from "./commands/init"
+// import { test } from "./commands/testcmd"
 
 const program = new Command()
 
 program.addCommand(add).addCommand(init)
+// .addCommand(test)
 
 program.parse(process.argv)
